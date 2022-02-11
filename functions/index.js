5214377d4ff640ea3ab298a4a7a6694f5cbf54a7
TheWisePigeon <zozozozeph@gmail.com>
2022-02-11 11:40:04 +0000

    created function to store users hash

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,28 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const keys = require("./keys");
+const serviceAccount = require("./admin.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
+const db = admin.firestore();
 exports.sayHello = functions.https.onCall((data, context)=>{
   return "Hi";
+});
+
+exports.test = functions.https.onCall((data, context)=>{
+  let success = Boolean;
+  const docRef = db.collection("USers").doc("brr").set({
+    hash: data.hash,
+  }).then(()=>{
+    success = true;
+  }).catch((error)=>{
+    success = false;
+  });
+  let message;
+  return ( message = success? "User created" : "There was an error");
 });
